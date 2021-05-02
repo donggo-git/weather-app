74e1c4ac3898671d5b1b2c437db588fada6753f0
Dong <Dlttthvlhhth@gmail.com>
2021-05-01 22:59:16 -0700

    add style for header

--- src/Header.js
+++ src/Header.js
@@ -8,10 +8,10 @@ function Header() {
             <h1>52<span>o</span></h1>
             <div className="Header__degreeContainer">
                 <div className="Header__degreeC">
-                    <p>C: 61<span>o</span></p>
+                    <p>C:61<span>o</span></p>
                 </div>
                 <div className="Header__degreeT">
-                    <p>T: 46<span>o</span></p>
+                    <p>T:46<span>o</span></p>
                 </div>
             </div>
         </div>
