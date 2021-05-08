30e095aab1a532b4b48904196a0da483724fc569
Dong <Dlttthvlhhth@gmail.com>
2021-05-07 22:41:30 -0700

    request data base on location

--- src/App.js
+++ src/App.js
@@ -1,17 +1,10 @@
 import React, { useState, useEffect } from 'react'
-import axios from 'axios'
 import './App.css'
 import HomePage from './HomePage';
 function App() {
-  useEffect(() => {
-    async function fetchData() {
-      const request = await axios.get("http://api.weatherapi.com/v1/current.json?key=dfa464158af4491f8e451132213004&q=Seattle&aqi=no");
-      console.log(request.data.current)
-      return request
-    }
-    fetchData()
-  }, [])
   const [location, setLocation] = useState(["Seattle"])
+
+
   const addLocation = (e) => {
     console.log(e)
     if (e === "") {
--- src/HomePage.js
+++ src/HomePage.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import LocationForm from './LocationForm';
 import Banner from './Banner';
+import axios from 'axios'
 
-function homePage(props) {
-    console.log(props.locations);
+function HomePage(props) {
+    useEffect(() => {
+        async function fetchData() {
+
+            const request = await Promise.all(props.locations.map(location => {
+                return axios.get(`http://api.weatherapi.com/v1/current.json?key=dfa464158af4491f8e451132213004&q=${location}&aqi=no`);
+            }))
+            console.log(request)
+            return request
+        }
+        fetchData()
+    }, [props.locations])
     return (
         <div>
             <LocationForm addLocation={props.addLocation} />
@@ -17,4 +28,4 @@ function homePage(props) {
     )
 }
 
-export default homePage
+export default HomePage
