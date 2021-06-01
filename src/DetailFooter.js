dd3cd5781bd8aa3f843eaabb6a0c3e6c0040b337
Dong <Dlttthvlhhth@gmail.com>
2021-05-31 20:00:16 -0700

    add DetailFooterLine.css

--- src/DetailFooter.js
+++ src/DetailFooter.js
@@ -7,15 +7,15 @@ function DetailFooter({ forecastday }) {
             {/*sun set and sun rise*/}
             < DetailFooterLine
                 leftTitle={"Sun rise"}
-                leftNumber={forecastday.astro.sunrise}
+                leftNumber={forecastday?.astro?.sunrise}
                 rightTitle={"Sun set"}
-                rightNumber={forecastday.astro.sunset}
+                rightNumber={forecastday?.astro?.sunset}
             />
             < DetailFooterLine
                 leftTitle={"Rain chance"}
-                leftNumber={forecastday.day.daily_chance_of_rain + "%"}
+                leftNumber={forecastday?.day?.daily_chance_of_rain + "%"}
                 rightTitle={"Snow chance"}
-                rightNumber={forecastday.day.daily_chance_of_snow + "%"}
+                rightNumber={forecastday?.day?.daily_chance_of_snow + "%"}
             />
             <div className="detailFooter_line"></div>
             <div className="detailFooter_line"></div>
--- src/DetailFooterLine.js
+++ src/DetailFooterLine.js
@@ -1,13 +1,14 @@
 import React from 'react'
+import './DetailFooterLine.css'
 
 function DetailFooterLine({ rightTitle, rightNumber, leftTitle, leftNumber }) {
     return (
         <div className="detailFooter_line">
-            <div className="detailFooter_line_left">
+            <div className="detailFooter_line_side">
                 <p className="title">{leftTitle}</p>
                 <p className="number">{leftNumber}</p>
             </div>
-            <div className="detailFooter_line_right">
+            <div className="detailFooter_line_side">
                 <p className="title">{rightTitle}</p>
                 <p className="number">{rightNumber}</p>
             </div>
