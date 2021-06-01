107940d439849c3445b5153f43260bcca3c81af9
Dong <Dlttthvlhhth@gmail.com>
2021-05-31 22:04:24 -0700

    fix the checkCondition

--- src/DetailPage.js
+++ src/DetailPage.js
@@ -4,24 +4,38 @@ import HeaderDetailPage from './HeaderDetailPage';
 import HourTempToday from './HourTempToday';
 import Forecast3Days from './Forecast3Days';
 import DetailFooter from './DetailFooter';
-import DetailFooterLine from './DetailFooterLine';
 
 function DetailPage({ detailData, detailLocation }) {
+    let hour = new Date().getHours();
+    let IsMorning = hour >= 12 ? true : false;
     let sunnyBackground = 'https://www.androidpolice.com/wp-content/uploads/2014/06/nexusae0_bg_weather_sunny_day.jpg';
     let rainBackground = 'https://ak7.picdn.net/shutterstock/videos/9915767/thumb/1.jpg';
-    let [backgroundImgLink, setBackgroundImgLink] = useState();
+    let nightBackground = 'https://wallpapercave.com/wp/wp2077619.jpg'
+    let rainNightBackground = 'https://wallpapercave.com/wp/wp2529651.jpg'
+    let sunset = parseInt(detailData?.forecast?.forecastday[0]?.astro?.sunset.split(":")[0])
     const checkCondition = (condition) => {
-        if (condition == "Partly cloudy" || condition == "Sunny" || condition == "Clear") {
-            return sunnyBackground;
+        console.log(hour - 12)
+        if (IsMorning ? (hour - 12 > sunset) : false) {
+            if (condition == "Partly cloudy" || condition == "Sunny" || condition == "Clear") {
+                return nightBackground;
+            }
+            else {
+                return rainNightBackground;
+            }
         }
         else {
-            return rainBackground
+            if (condition == "Partly cloudy" || condition == "Sunny" || condition == "Clear") {
+                return sunnyBackground;
+            }
+            else {
+                return rainBackground
+            }
         }
     }
     let backgroundStyle = {
         backgroundImage: `url(${checkCondition(detailData?.current?.condition?.text)})`
     }
-    console.log(detailData);
+
     return (
         <div
             className="DetailPage"
