c258a4b3eea2be5432134bb4b6f1dfc2f5a6a29e
Dong <dlttthvlhhth@gmail.com>
2022-06-16 17:35:14 -0700

    add ProgressBar for percent rain and snow in DetailFooterLine.js

--- src/DetailPage/DetailFooterLine.js
+++ src/DetailPage/DetailFooterLine.js
@@ -1,16 +1,33 @@
 import React from 'react'
 import './DetailFooterLine.css'
+import ProgressBar from 'react-percent-bar';
 
 function DetailFooterLine({ rightTitle, rightNumber, leftTitle, leftNumber }) {
+    console.log(Number(leftNumber))
+    const convertPercentToNumber = function (percent) {
+        console.log(percent)
+        const convertedNumber = Number(percent?.split('%')[0])
+        console.log(convertedNumber)
+        return convertedNumber;
+    }
     return (
         <div className="detailFooter_line">
             <div className="detailFooter_line_side">
                 <p className="title">{leftTitle}</p>
-                <p className="detailFooter_line_number">{leftNumber}</p>
+                {//if the title is percent chance use the progress bar
+                    leftTitle == "Rain chance" || leftNumber == "Snow chance" ?
+                        <ProgressBar colorShift={true} fillColor="orange" percent={convertPercentToNumber(leftNumber)} /> :
+                        <p className="detailFooter_line_number">{leftNumber}</p>
+                }
+
             </div>
             <div className="detailFooter_line_side">
                 <p className="title">{rightTitle}</p>
-                <p className="detailFooter_line_number">{rightNumber}</p>
+                {//if the title is percent chance use the progress bar
+                    rightTitle == "Rain chance" || rightTitle == "Snow chance" ?
+                        <ProgressBar colorShift={true} fillColor="orange" percent={convertPercentToNumber(rightNumber)} /> :
+                        <p className="detailFooter_line_number">{rightNumber}</p>
+                }
             </div>
         </div>
     )
--- src/DetailPage/Forecast3Days.js
+++ src/DetailPage/Forecast3Days.js
@@ -7,15 +7,15 @@ function Predit7Day({ forecastday }) {
         return arrDate.reverse().join("/")
     }
     return (
-        <div className="Forecast3Days_container">
+        <div className="Forecast3Days__container">
             {forecastday ? forecastday.map((day, index) => (
                 <div className="Forecast1Day" key={index}>
-                    <div className="Forecast1Day_date">{modifyDate(day.date)}</div>
-                    <div className="Forecast1Day_img_container">
+                    <div className="Forecast1Day__date">{modifyDate(day.date)}</div>
+                    <div className="Forecast1Day__img--container">
                         <img src={day.day.condition.icon} />
                     </div>
-                    <div className="maxTemp">{day.day.maxtemp_f}</div>
-                    <div className="minTemp">{day.day.mintemp_f}</div>
+                    <div className="Forecast1Day__maxTemp Forecast1Day__Temp">{day.day.maxtemp_f}<span>o</span></div>
+                    <div className="Forecast1Day__minTemp Forecast1Day__Temp">{day.day.mintemp_f}<span>o</span></div>
                 </div>
             )) : undefined}
         </div>
